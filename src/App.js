11c706c22b3d87ac2705fc5090e63fe8a4403112
carylvallejo <carylchrisabelle.vallejo@benilde.edu.ph>
2022-09-09 00:44:47 +0800

    Get images commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//const url = 'https://api.thecatapi.com/v1/images/search'
+
+const App = () => {
+	// console.log('Hello World test')
+	const [ cats, setCats ] = useState('') 
+
+		useEffect( () => {
+			haveCat()
+		}, [])
+
+	const haveCat = () => {
+		axios
+				.get('https://api.thecatapi.com/v1/images/search')
+				.then(response => {
+					console.log('Cats: ', response.data[0].url)
+					//setCats(response[0].data)
+					const cats = response.data[0].url
+					setCats(cats)
+				})
+	}
+
+	const showDetails = (event) => {
+    setCats(event.target.value)
+  }
+	
+	return (
+		<div>
+			<h1>Get cat images</h1>
+			<img src={cats} alt="cat" height="300px" width="250px"/>
+			<button onClick={haveCat}>Give me a new cat</button>
+		</div>
+ 	)
 }
 
 export default App;
